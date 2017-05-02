654b4825767d3d24d971d80324ff35b0acbe5f66
Jeremiah Burley <jeremiah@techburley.com>
2017-05-01 23:40:56 -0700

    Remove comment

--- Solutions/src/solutions/PE001.java
+++ Solutions/src/solutions/PE001.java
@@ -14,7 +14,7 @@ public class PE001 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         // Define Local Variables
         List<Integer> multiplesFound = new ArrayList<>();
         int sum = 0;
