baf6bf4d4a7acd7a1096bdfb5e2f33282aaa6238
Jeremiah Burley <jeremiah@techburley.com>
2017-05-01 23:33:34 -0700

    Solution Commit, try1

---
+++ Solutions/src/solutions/PE001.java
@@ -0,0 +1,35 @@
+/*
+    Problem Statement:
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+    Date: 5/1/2017
+    Author: Jeremiah Burley
+*/
+package solutions;
+import java.util.*;
+
+public class PE001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Define Local Variables
+        List<Integer> multiplesFound = new ArrayList<>();
+        int sum = 0;
+        
+        for (int i = 1; i < 1000; i++) {
+            
+            if ((i % 3 == 0) || (i % 5 == 0)) {
+                multiplesFound.add(i);        
+            }
+        }
+        
+        for (int i = 0; i < multiplesFound.size(); i++) {
+            sum += multiplesFound.get(i);
+        }
+        
+        System.out.println("The Value is: " + sum);
+    }
+}
